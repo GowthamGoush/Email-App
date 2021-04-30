2e4c926f1d29094f9daf9f4a49831e1f169c3067
GowthamGoush <westcity.21.gowtham@gmail.com>
2021-04-30 16:27:05 +0530

    Add register activity

---
+++ app/src/main/java/com/example/email_app/Activities/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.example.email_app.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.email_app.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
