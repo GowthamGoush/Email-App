1571006627dbae5c60008b47f55c57dbb1f7637b
GowthamGoush <westcity.21.gowtham@gmail.com>
2021-04-30 18:25:44 +0530

    Implement splash screen

--- app/src/main/java/com/example/email_app/SplashScreen.java
+++ app/src/main/java/com/example/email_app/SplashScreen.java
@@ -2,7 +2,11 @@ package com.example.email_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.email_app.Activities.RegisterActivity;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -10,5 +14,12 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, RegisterActivity.class));
+            }
+        },2000);
     }
 }
