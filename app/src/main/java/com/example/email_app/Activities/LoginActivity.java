5f30a86f33cf27fffd9011c5d9855d99e1e98ed6
GowthamGoush <westcity.21.gowtham@gmail.com>
2021-05-04 09:02:56 +0530

    Add login activity

---
+++ app/src/main/java/com/example/email_app/Activities/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.email_app.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.email_app.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
