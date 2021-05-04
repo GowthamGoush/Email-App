eeaba63fcac78072c0ed5a9cb422d961ca48282a
GowthamGoush <westcity.21.gowtham@gmail.com>
2021-05-04 10:53:32 +0530

    Add intent helper class

---
+++ app/src/main/java/com/example/email_app/Utils/IntentHelper.java
@@ -0,0 +1,22 @@
+package com.example.email_app.Utils;
+
+import android.content.Context;
+import android.content.Intent;
+import com.example.email_app.Activities.LoginActivity;
+import com.example.email_app.Activities.RegisterActivity;
+
+public class IntentHelper {
+    Context context;
+
+    public IntentHelper(Context context) {
+        this.context = context;
+    }
+
+    public void GoToRegister() {
+        context.startActivity(new Intent(context, RegisterActivity.class));
+    }
+
+    public void GoToLogin() {
+        context.startActivity(new Intent(context, LoginActivity.class));
+    }
+}
