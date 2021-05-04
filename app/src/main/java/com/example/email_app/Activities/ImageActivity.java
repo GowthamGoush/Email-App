43ee9c5c0933b1b12291ded482bf6f798c56a21c
GowthamGoush <westcity.21.gowtham@gmail.com>
2021-05-05 00:09:55 +0530

    Add simple click feature

--- app/src/main/java/com/example/email_app/Activities/ImageActivity.java
+++ app/src/main/java/com/example/email_app/Activities/ImageActivity.java
@@ -3,12 +3,43 @@ package com.example.email_app.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class ImageActivity extends AppCompatActivity {
+import com.airbnb.lottie.LottieAnimationView;
+import com.example.email_app.R;
+import com.example.email_app.Utils.IntentHelper;
+
+public class ImageActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private LottieAnimationView lottieAnimationView;
+    private TextView returnBackText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image);
+
+        lottieAnimationView = findViewById(R.id.lottieImage);
+        lottieAnimationView.setOnClickListener(this);
+
+        returnBackText = findViewById(R.id.returnText);
+        returnBackText.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.lottieImage:
+                Toast.makeText(ImageActivity.this, "You have clicked!", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.returnText:
+                IntentHelper intentHelper = new IntentHelper(ImageActivity.this);
+                intentHelper.GoToLogin();
+                finish();
+                break;
+        }
     }
 }
