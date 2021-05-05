7b892a73fb819b79d31d47d997c34b50084c6409
GowthamGoush <westcity.21.gowtham@gmail.com>
2021-05-05 18:04:42 +0530

    Add feature to switch primary screen

--- app/src/main/java/com/example/email_app/SplashScreen.java
+++ app/src/main/java/com/example/email_app/SplashScreen.java
@@ -9,21 +9,29 @@ import android.os.Handler;
 import com.example.email_app.Activities.LoginActivity;
 import com.example.email_app.Activities.RegisterActivity;
 import com.example.email_app.Utils.IntentHelper;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SplashScreen extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                IntentHelper intentHelper = new IntentHelper(SplashScreen.this);
+        mAuth = FirebaseAuth.getInstance();
+
+        IntentHelper intentHelper = new IntentHelper(SplashScreen.this);
+
+        new Handler().postDelayed(() -> {
+            if (mAuth.getCurrentUser() == null){
                 intentHelper.GoToRegister();
-                finish();
+            } else {
+                intentHelper.GoToImage();
             }
+            finish();
         },2200);
+
     }
 }
