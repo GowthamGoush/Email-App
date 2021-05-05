fdcc10880cdf24ae562594b82e2414f1504b1096
GowthamGoush <westcity.21.gowtham@gmail.com>
2021-05-05 14:14:26 +0530

    Add email scheduler

--- app/src/main/java/com/example/email_app/Activities/ImageActivity.java
+++ app/src/main/java/com/example/email_app/Activities/ImageActivity.java
@@ -2,6 +2,10 @@ package com.example.email_app.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -9,12 +13,15 @@ import android.widget.Toast;
 
 import com.airbnb.lottie.LottieAnimationView;
 import com.example.email_app.R;
+import com.example.email_app.Receivers.Email;
 import com.example.email_app.Utils.IntentHelper;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ImageActivity extends AppCompatActivity implements View.OnClickListener{
 
     private LottieAnimationView lottieAnimationView;
     private TextView returnBackText;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,15 +34,35 @@ public class ImageActivity extends AppCompatActivity implements View.OnClickList
         returnBackText = findViewById(R.id.returnText);
         returnBackText.setOnClickListener(this);
 
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+
+    private void sendEmail(){
+
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+        Intent intent = new Intent(this, Email.class);
+        intent.putExtra("UserEmail",mAuth.getCurrentUser().getEmail());
+
+        int requestCode = (int) System.currentTimeMillis();
+
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, requestCode, intent, 0);
+
+        alarmManager.set(AlarmManager.RTC, R.string.email_trigger_time, pendingIntent);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
+
             case R.id.lottieImage:
-                Toast.makeText(ImageActivity.this, "You have clicked!", Toast.LENGTH_LONG).show();
+                sendEmail();
+                Toast.makeText(ImageActivity.this, "A mail will be sent in 5 minutes!", Toast.LENGTH_LONG).show();
                 break;
+
             case R.id.returnText:
+                mAuth.signOut();
                 IntentHelper intentHelper = new IntentHelper(ImageActivity.this);
                 intentHelper.GoToLogin();
                 finish();
